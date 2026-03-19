a6c15c444ea434c632db61517883134e29888a6a
Ferofranki <mumanenko_saveliy@mail.ru>
2026-03-19 12:10:28 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace PWLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnZapisz = new System.Windows.Forms.Button();
+            this.btnOdczyt = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnUsun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(353, 313);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnZapisz
+            // 
+            this.btnZapisz.Location = new System.Drawing.Point(12, 357);
+            this.btnZapisz.Name = "btnZapisz";
+            this.btnZapisz.Size = new System.Drawing.Size(125, 49);
+            this.btnZapisz.TabIndex = 1;
+            this.btnZapisz.Text = "Zapisz do .csv";
+            this.btnZapisz.UseVisualStyleBackColor = true;
+            // 
+            // btnOdczyt
+            // 
+            this.btnOdczyt.Location = new System.Drawing.Point(237, 357);
+            this.btnOdczyt.Name = "btnOdczyt";
+            this.btnOdczyt.Size = new System.Drawing.Size(128, 49);
+            this.btnOdczyt.TabIndex = 2;
+            this.btnOdczyt.Text = "Odczytuj z .csv";
+            this.btnOdczyt.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(389, 117);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(104, 49);
+            this.btnDodaj.TabIndex = 3;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // btnUsun
+            // 
+            this.btnUsun.Location = new System.Drawing.Point(391, 195);
+            this.btnUsun.Name = "btnUsun";
+            this.btnUsun.Size = new System.Drawing.Size(104, 53);
+            this.btnUsun.TabIndex = 4;
+            this.btnUsun.Text = "Usun";
+            this.btnUsun.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 418);
+            this.Controls.Add(this.btnUsun);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnOdczyt);
+            this.Controls.Add(this.btnZapisz);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnZapisz;
+        private System.Windows.Forms.Button btnOdczyt;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnUsun;
+    }
+}
+
---
+++ NowyPracownik.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace PWLab3
+{
+    partial class NowyPracownik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbName = new System.Windows.Forms.ComboBox();
+            this.cbSurname = new System.Windows.Forms.ComboBox();
+            this.cbAge = new System.Windows.Forms.ComboBox();
+            this.cbStanowisko = new System.Windows.Forms.ComboBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbSurname = new System.Windows.Forms.Label();
+            this.lbAge = new System.Windows.Forms.Label();
+            this.lbStanowisko = new System.Windows.Forms.Label();
+            this.btnZatwierdz = new System.Windows.Forms.Button();
+            this.btnAnuluj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbName
+            // 
+            this.cbName.FormattingEnabled = true;
+            this.cbName.Location = new System.Drawing.Point(13, 13);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(121, 24);
+            this.cbName.TabIndex = 0;
+            // 
+            // cbSurname
+            // 
+            this.cbSurname.FormattingEnabled = true;
+            this.cbSurname.Location = new System.Drawing.Point(13, 44);
+            this.cbSurname.Name = "cbSurname";
+            this.cbSurname.Size = new System.Drawing.Size(121, 24);
+            this.cbSurname.TabIndex = 1;
+            // 
+            // cbAge
+            // 
+            this.cbAge.FormattingEnabled = true;
+            this.cbAge.Location = new System.Drawing.Point(13, 75);
+            this.cbAge.Name = "cbAge";
+            this.cbAge.Size = new System.Drawing.Size(121, 24);
+            this.cbAge.TabIndex = 2;
+            // 
+            // cbStanowisko
+            // 
+            this.cbStanowisko.FormattingEnabled = true;
+            this.cbStanowisko.Location = new System.Drawing.Point(13, 106);
+            this.cbStanowisko.Name = "cbStanowisko";
+            this.cbStanowisko.Size = new System.Drawing.Size(121, 24);
+            this.cbStanowisko.TabIndex = 3;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(157, 16);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(32, 16);
+            this.lbName.TabIndex = 4;
+            this.lbName.Text = "Imie";
+            // 
+            // lbSurname
+            // 
+            this.lbSurname.AutoSize = true;
+            this.lbSurname.Location = new System.Drawing.Point(157, 44);
+            this.lbSurname.Name = "lbSurname";
+            this.lbSurname.Size = new System.Drawing.Size(65, 16);
+            this.lbSurname.TabIndex = 5;
+            this.lbSurname.Text = "Nazwisko";
+            // 
+            // lbAge
+            // 
+            this.lbAge.AutoSize = true;
+            this.lbAge.Location = new System.Drawing.Point(160, 75);
+            this.lbAge.Name = "lbAge";
+            this.lbAge.Size = new System.Drawing.Size(38, 16);
+            this.lbAge.TabIndex = 6;
+            this.lbAge.Text = "Wiek";
+            // 
+            // lbStanowisko
+            // 
+            this.lbStanowisko.AutoSize = true;
+            this.lbStanowisko.Location = new System.Drawing.Point(160, 106);
+            this.lbStanowisko.Name = "lbStanowisko";
+            this.lbStanowisko.Size = new System.Drawing.Size(76, 16);
+            this.lbStanowisko.TabIndex = 7;
+            this.lbStanowisko.Text = "Stanowisko";
+            // 
+            // btnZatwierdz
+            // 
+            this.btnZatwierdz.Location = new System.Drawing.Point(12, 153);
+            this.btnZatwierdz.Name = "btnZatwierdz";
+            this.btnZatwierdz.Size = new System.Drawing.Size(95, 40);
+            this.btnZatwierdz.TabIndex = 8;
+            this.btnZatwierdz.Text = "Zatwierdz";
+            this.btnZatwierdz.UseVisualStyleBackColor = true;
+            // 
+            // btnAnuluj
+            // 
+            this.btnAnuluj.Location = new System.Drawing.Point(134, 153);
+            this.btnAnuluj.Name = "btnAnuluj";
+            this.btnAnuluj.Size = new System.Drawing.Size(102, 40);
+            this.btnAnuluj.TabIndex = 9;
+            this.btnAnuluj.Text = "Anuluj";
+            this.btnAnuluj.UseVisualStyleBackColor = true;
+            // 
+            // NowyPracownik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 205);
+            this.Controls.Add(this.btnAnuluj);
+            this.Controls.Add(this.btnZatwierdz);
+            this.Controls.Add(this.lbStanowisko);
+            this.Controls.Add(this.lbAge);
+            this.Controls.Add(this.lbSurname);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.cbStanowisko);
+            this.Controls.Add(this.cbAge);
+            this.Controls.Add(this.cbSurname);
+            this.Controls.Add(this.cbName);
+            this.Name = "NowyPracownik";
+            this.Text = "NowyPracownik";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbName;
+        private System.Windows.Forms.ComboBox cbSurname;
+        private System.Windows.Forms.ComboBox cbAge;
+        private System.Windows.Forms.ComboBox cbStanowisko;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbSurname;
+        private System.Windows.Forms.Label lbAge;
+        private System.Windows.Forms.Label lbStanowisko;
+        private System.Windows.Forms.Button btnZatwierdz;
+        private System.Windows.Forms.Button btnAnuluj;
+    }
+}
